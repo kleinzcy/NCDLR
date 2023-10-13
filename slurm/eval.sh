3896984bf485a3c378f06bdf2aba60b2d6fd64fc
kleinzcy <zhangchy2@shanghaitech.edu.cn>
2023-10-13 10:22:16 +0800

    remove slurm

--- slurm/eval.sh
+++
@@ -1,4 +0,0 @@
-# CUDA_VISIBLE_DEVICES=0 python LA.py --c config/ours/inaturalist.yaml
-CUDA_VISIBLE_DEVICES=0 python LA.py --c config/UNO/inaturalist.yaml
-CUDA_VISIBLE_DEVICES=0 python LA.py --c config/AutoNovel/inaturalist.yaml
-
